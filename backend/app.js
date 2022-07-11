a0776a53c3f9ec235a4f38b0f4be275afe633468
Whitney Minson <whitnessprints@gmail.com>
2022-07-11 13:26:29 -0700

    Error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 const isProduction = environment === 'production';
 
@@ -20,17 +21,17 @@ app.use(express.json());
 if (!isProduction) {
     // enable cors only in development
     app.use(cors());
-  }
+}
   
   // helmet helps set a variety of headers to better secure your app
-  app.use(
+app.use(
     helmet.crossOriginResourcePolicy({ 
       policy: "cross-origin" 
     })
-  );
+);
   
   // Set the _csrf token and create req.csrfToken method
-  app.use(
+app.use(
     csurf({
       cookie: {
         secure: isProduction,
@@ -38,8 +39,39 @@ if (!isProduction) {
         httpOnly: true
       }
     })
-  );
+);
 
-  app.use(routes);
+app.use(routes);
 
-  module.exports = app;
+  // Catch unhandled requests and forwar to error handler:
+  app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
+
+module.exports = app;
